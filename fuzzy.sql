8b1549977adb0a678986479952ea6abd568be841
ayulestari10 <lestariayu669@gmail.com>
2017-10-28 11:07:19 +0700

    fuzzy.sql

---
+++ fuzzy.sql
@@ -0,0 +1,189 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 28, 2017 at 06:06 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fuzzy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bobot`
+--
+
+CREATE TABLE `bobot` (
+  `id_bobot` int(11) NOT NULL,
+  `id_kriteria` int(11) NOT NULL,
+  `fuzzy` varchar(100) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keputusan`
+--
+
+CREATE TABLE `keputusan` (
+  `id_keputusan` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `min` float NOT NULL,
+  `max` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kriteria`
+--
+
+CREATE TABLE `kriteria` (
+  `id_kriteria` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `benefit` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelamar`
+--
+
+CREATE TABLE `pelamar` (
+  `id_pelamar` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `tempat_lahir` varchar(40) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `no_hp` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `jk` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pelamar`
+--
+
+INSERT INTO `pelamar` (`id_pelamar`, `nama`, `alamat`, `tempat_lahir`, `tgl_lahir`, `no_hp`, `email`, `jk`) VALUES
+(1, 'Ayu', 'hah ahahaa', 'Lahat', '1998-08-10', '082372348888', 'lestariayu669@gmail.com', 'perempuan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penilaian`
+--
+
+CREATE TABLE `penilaian` (
+  `id_penilaian` int(11) NOT NULL,
+  `id_bobot` int(11) NOT NULL,
+  `id_kriteria` int(11) NOT NULL,
+  `id_pelamar` int(11) NOT NULL,
+  `hasil` float NOT NULL,
+  `keputusan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `bobot`
+--
+ALTER TABLE `bobot`
+  ADD PRIMARY KEY (`id_bobot`),
+  ADD KEY `id_kriteria` (`id_kriteria`);
+
+--
+-- Indexes for table `keputusan`
+--
+ALTER TABLE `keputusan`
+  ADD PRIMARY KEY (`id_keputusan`);
+
+--
+-- Indexes for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  ADD PRIMARY KEY (`id_kriteria`);
+
+--
+-- Indexes for table `pelamar`
+--
+ALTER TABLE `pelamar`
+  ADD PRIMARY KEY (`id_pelamar`);
+
+--
+-- Indexes for table `penilaian`
+--
+ALTER TABLE `penilaian`
+  ADD PRIMARY KEY (`id_penilaian`),
+  ADD KEY `id_bobot` (`id_bobot`),
+  ADD KEY `id_kriteria` (`id_kriteria`),
+  ADD KEY `id_pelamar` (`id_pelamar`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bobot`
+--
+ALTER TABLE `bobot`
+  MODIFY `id_bobot` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `keputusan`
+--
+ALTER TABLE `keputusan`
+  MODIFY `id_keputusan` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pelamar`
+--
+ALTER TABLE `pelamar`
+  MODIFY `id_pelamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `penilaian`
+--
+ALTER TABLE `penilaian`
+  MODIFY `id_penilaian` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
