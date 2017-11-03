d4b083e6e75c8bb0a32b41380ba5df0d2b23d028
ayulestari10 <lestariayu669@gmail.com>
2017-11-03 20:11:23 +0700

    view pendafataran

--- application/views/daftar.php
+++ application/views/daftar.php
@@ -1,43 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Website Percetakan Sarana Palembang">
-    <meta name="author" content="Marina Adhitia">
 
-    <title>Pendaftaran Pelamar | Fuzzy</title>
+    <title><?= $title ?></title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="<?= base_url('assets/sbadmin-2') ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="<?= base_url('assets') ?>/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="<?= base_url('assets') ?>/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="<?= base_url('assets') ?>/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- bootstrap-daterangepicker -->
+    <link href="<?= base_url('assets') ?>/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
 
-    <!-- MetisMenu CSS -->
-    <link href="<?= base_url('assets/sbadmin-2') ?>/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <!-- Custom Theme Style -->
+    <link href="<?= base_url('assets') ?>/build/css/custom.min.css" rel="stylesheet">
+  </head>
 
-    <!-- Custom CSS -->
-    <link href="<?= base_url('assets/sbadmin-2') ?>/dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="<?= base_url('assets/sbadmin-2') ?>/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <link rel="icon" type="image/x-icon" href="">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-    <div class="container">
+  <body class="nav-md" style="background: white;">
+        <div class="container">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1" style="margin-top: -15%;">
+            <div class="col-md-10 col-md-offset-1" style="margin-top:5%;">
                 <div class="login-panel panel panel-primary">
                     <div class="panel-heading">
                         <h3 class="panel-title">Selamat Datang di Website Pendaftaran Blabla</h3>
@@ -77,7 +65,7 @@
                                         <textarea class="form-control" rows="3" name="alamat" required></textarea>
                                     </div>
                                     <!-- Change this to a button or input when using this as a form -->
-                                    <input type="submit" name="login-submit" value="Daftar" class="btn btn-lg btn-success btn-block">
+                                    <input type="submit" name="login-submit" value="Daftar" class="btn btn-lg btn-primary btn-block">
                                 <?= form_close() ?>
                             </div>
                         </div>
@@ -86,19 +74,51 @@
             </div>
         </div>
     </div>
+  </body>
+
+
+<!-- footer content -->
+        <footer>
+          <div class="row" style="text-align: center;">
+            <div class="col-md-8 col-md-offset-1">
+                Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+            </div>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
 
     <!-- jQuery -->
-    <script src="<?= base_url('assets/sbadmin-2') ?>/vendor/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="<?= base_url('assets/sbadmin-2') ?>/vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="<?= base_url('assets/sbadmin-2') ?>/vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="<?= base_url('assets/sbadmin-2') ?>/dist/js/sb-admin-2.js"></script>
-
-</body>
-
+    <script src="<?= base_url('assets') ?>/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="<?= base_url('assets') ?>/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="<?= base_url('assets') ?>/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="<?= base_url('assets') ?>/vendors/nprogress/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="<?= base_url('assets') ?>/vendors/Chart.js/dist/Chart.min.js"></script>
+    <!-- jQuery Sparklines -->
+    <script src="<?= base_url('assets') ?>/vendors/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+    <!-- Flot -->
+    <script src="<?= base_url('assets') ?>/vendors/Flot/jquery.flot.js"></script>
+    <script src="<?= base_url('assets') ?>/vendors/Flot/jquery.flot.pie.js"></script>
+    <script src="<?= base_url('assets') ?>/vendors/Flot/jquery.flot.time.js"></script>
+    <script src="<?= base_url('assets') ?>/vendors/Flot/jquery.flot.stack.js"></script>
+    <script src="<?= base_url('assets') ?>/vendors/Flot/jquery.flot.resize.js"></script>
+    <!-- Flot plugins -->
+    <script src="<?= base_url('assets') ?>/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+    <script src="<?= base_url('assets') ?>/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+    <script src="<?= base_url('assets') ?>/vendors/flot.curvedlines/curvedLines.js"></script>
+    <!-- DateJS -->
+    <script src="<?= base_url('assets') ?>/vendors/DateJS/build/date.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="<?= base_url('assets') ?>/vendors/moment/min/moment.min.js"></script>
+    <script src="<?= base_url('assets') ?>/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+    
+    <!-- Custom Theme Scripts -->
+    <script src="<?= base_url('assets') ?>/build/js/custom.min.js"></script>
+  </body>
 </html>
