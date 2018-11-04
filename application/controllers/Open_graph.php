03338c9ec1239db4aced10b711762e4ef77a0aea
azhry <arliansyah_azhary@yahoo.com>
2018-11-04 21:27:21 +0700

    fix image open graph

--- application/controllers/Open_graph.php
+++ application/controllers/Open_graph.php
@@ -15,7 +15,7 @@ class Open_graph extends MY_Controller
 		$this->data['title']		= $this->data['barang']->nama;
 		$this->data['description']	= $this->data['barang']->deskripsi;
 		$this->data['kategori']		= $this->kategori_barang_m->get_row(['id_kategori_barang' => $this->data['barang']->id_kategori_barang])->nama_kategori;
-		$this->data['img']			= base_url('assets/produk/' . $this->data['kategori'] . '/' . $this->data['barang']->kode_barang . '.JPG');
+		$this->data['img']			= base_url('assets/produk/' . str_replace(' ', '%20', $this->data['kategori']) . '/' . $this->data['barang']->kode_barang . '.JPG');
 		$this->load->view( 'open_graph', $this->data );
 	}
 }
